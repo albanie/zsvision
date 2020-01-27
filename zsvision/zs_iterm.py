5c81233cd272819e5e75c3e5774f37d02c297471
albanie <albanie@robots.ox.ac.uk>
2020-01-27 07:25:49 +0000

    swtich to png default to avoid iterm2 issues

--- zsvision/zs_iterm.py
+++ zsvision/zs_iterm.py
@@ -4,17 +4,14 @@ import matplotlib.pyplot as plt
 
 IMGCAT = os.path.expanduser('~/local/bin/imgcat')
 
-def zs_dispFig(imgcat_path=IMGCAT):
-    """
-    shows a matplotlib plot inline in iTerm by saving
-    it to a temporary file, displaying the file and
-    then cleaning up.
 
-    Note: This function requires the use of an iTerm
-    terminal (available from https://www.iterm2.com)
-    and requires that the `imgcat` script is on your
-    $PATH. It can be useful when running MATLAB on a
-    server when you don't have access to a GUI to
+def zs_dispFig(imgcat_path=IMGCAT):
+    """Shows a matplotlib plot inline in iTerm by saving it to a temporary file,
+    displaying the file and then cleaning up.
+
+    Note: This function requires the use of an iTerm terminal (available from
+    https://www.iterm2.com) and requires that the `imgcat` script is on your $PATH. It
+    can be useful when running code on a server when you don't have access to a GUI to
     display figures.
 
     Copyright (C) 2016 Samuel Albanie
@@ -22,14 +19,8 @@ def zs_dispFig(imgcat_path=IMGCAT):
     """
 
     # save figure as temp image
-    im_name = '_tmp.jpeg'
-
-    # switch to png if jpeg is unsupported
-    try:
-        plt.savefig(im_name)
-    except ValueError:
-        im_name = '_tmp.png'
-        plt.savefig(im_name)
+    im_name = '_tmp.png'
+    plt.savefig(im_name)
 
     # display in iterm
     subprocess.call([imgcat_path, "--depth", "iterm2", im_name])
